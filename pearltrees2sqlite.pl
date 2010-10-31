d22271963f17d1bb2c489df4cad4e023669e3ba3
Emmanuel Di Pretoro <edipretoro@gmail.com>
2010-10-31 21:19:17 +0100

    * pearltrees2sqlite.pl: remove the creation of classes with Class:DBI and replace them with the DBIx::Class-based class

--- pearltrees2sqlite.pl
+++ pearltrees2sqlite.pl
@@ -1,6 +1,105 @@
 #!/usr/bin/env perl
+
 use warnings;
 use strict;
+
+package Bookmarks::Schema;
+use base 'DBIx::Class::Schema';
+
+__PACKAGE__->load_namespaces();
+
+package Bookmarks::Schema::Result::Article;
+use base 'DBIx::Class';
+
+__PACKAGE__->load_components(qw/ Core /);
+__PACKAGE__->table('article');
+
+__PACKAGE__->add_columns(
+    'artID' => {
+        'data_type'         => 'bigint',
+        'is_auto_increment' => 1,
+        'default_value'     => undef,
+        'is_foreign_key'    => 0,
+        'name'              => 'artID',
+        'is_nullable'       => 0,
+        'size'              => '20'
+    },
+    'URL' => {
+        'data_type'         => 'text',
+        'is_auto_increment' => 0,
+        'default_value'     => undef,
+        'is_foreign_key'    => 0,
+        'name'              => 'URL',
+        'is_nullable'       => 0,
+    },
+    'Title' => {
+        'data_type'         => 'text',
+        'is_auto_increment' => 0,
+        'default_value'     => undef,
+        'is_foreign_key'    => 0,
+        'name'              => 'Title',
+        'is_nullable'       => 0,
+    },
+);
+
+__PACKAGE__->set_primary_key( 'artID' );
+
+package Bookmarks::Schema::Result::Tag;
+use base 'DBIx::Class';
+
+__PACKAGE__->load_components(qw/ Core /);
+__PACKAGE__->table('tag');
+
+__PACKAGE__->add_columns(
+    'tagID' => {
+        'data_type'         => 'bigint',
+        'is_auto_increment' => 1,
+        'default_value'     => undef,
+        'is_foreign_key'    => 0,
+        'name'              => 'tagID',
+        'is_nullable'       => 0,
+        'size'              => '20'
+    },
+    'Tag' => {
+        'data_type'         => 'text',
+        'is_auto_increment' => 0,
+        'default_value'     => undef,
+        'is_foreign_key'    => 0,
+        'name'              => 'Tag',
+        'is_nullable'       => 0,
+    },
+);
+
+__PACKAGE__->set_primary_key( 'tagID' );
+
+package Bookmarks::Schema::Result::TagLinks;
+use base 'DBIx::Class';
+
+__PACKAGE__->load_components(qw/ Core /);
+__PACKAGE__->table('taglinks');
+
+__PACKAGE__->add_columns(
+    'tagID' => {
+        'data_type'         => 'bigint',
+        'is_auto_increment' => 0,
+        'default_value'     => undef,
+        'is_foreign_key'    => 0,
+        'name'              => 'tagID',
+        'is_nullable'       => 0,
+        'size'              => '20'
+    },
+    'artID' => {
+        'data_type'         => 'bigint',
+        'is_auto_increment' => 0,
+        'default_value'     => undef,
+        'is_foreign_key'    => 1,
+        'name'              => 'artID',
+        'is_nullable'       => 0,
+        'size'              => '20'
+    },
+);
+
+package main;
 
 use XML::LibXML;
 use Data::Dump;
@@ -64,26 +163,9 @@ foreach my $pearl (@pearls) {
     @taggedpearl{@url} = @tag;
 }
 
-package Bookmarks::DBI;
-use base 'Class::DBI';
-
-my $dbfile = $config->{database};
-Bookmarks::DBI->connection( "dbi:SQLite:dbname=$dbfile", "", "" );
-
-package Bookmarks::Article;
-use base 'Bookmarks::DBI';
-Bookmarks::Article->table('article');
-Bookmarks::Article->columns( All => qw/artID URL Title/ );
-
-package Bookmarks::Tag;
-use base 'Bookmarks::DBI';
-Bookmarks::Tag->table('tag');
-Bookmarks::Tag->columns( All => qw/tagID Tag/ );
-
-package Bookmarks::Taglinks;
-use base 'Bookmarks::DBI';
-Bookmarks::Taglinks->table('tagLinks');
-Bookmarks::Taglinks->columns( All => qw/artID tagID/ );
+my $deploy = -e $config->{database} ? 0 : 1;
+my $schema = Bookmarks::Schema->connect( 'dbi:SQLite:dbname=' . $config->{database}, { AutoCommit => 1 } );
+$schema->deploy() if $deploy;
 
 my $i = 0;
 my %sites;
@@ -113,25 +195,25 @@ foreach my $site ( keys(%pearl) ) {
     }
 }
 
-my $fts3 = DBI->connect( "dbi:SQLite:dbname=$dbfile", "", "" );
-$fts3->do(<<"") or die DBI::errstr;
-CREATE VIRTUAL TABLE webpage USING fts3 (pgContent TEXT, artID INTEGER NOT NULL REFERENCES article.artID)
+# my $fts3 = DBI->connect( "dbi:SQLite:dbname=$dbfile", "", "" );
+# $fts3->do(<<"") or die DBI::errstr;
+# CREATE VIRTUAL TABLE webpage USING fts3 (pgContent TEXT, artID INTEGER NOT NULL REFERENCES article.artID)
 
-foreach my $artID ( keys(%sites) ) {
-    my $link = LWP::Simple::get("$sites{$artID}");
-    if ( defined($link) ) {
-        my $insert =
-          $fts3->prepare("INSERT INTO webpage(pgContent) VALUES (?)");
-        my $newdata  = $insert->execute($link);
-        my $insert2  = $fts3->prepare("INSERT INTO webpage(artID) VALUES (?)");
-        my $newdata2 = $insert2->execute($artID);
-    }
-    else {
-        open OUT, '>>', "error";
-        print( OUT "Error : Unable to reach $sites{$artID} \n" );
-        close(OUT);
-    }
-}
+# foreach my $artID ( keys(%sites) ) {
+#     my $link = LWP::Simple::get("$sites{$artID}");
+#     if ( defined($link) ) {
+#         my $insert =
+#           $fts3->prepare("INSERT INTO webpage(pgContent) VALUES (?)");
+#         my $newdata  = $insert->execute($link);
+#         my $insert2  = $fts3->prepare("INSERT INTO webpage(artID) VALUES (?)");
+#         my $newdata2 = $insert2->execute($artID);
+#     }
+#     else {
+#         open OUT, '>>', "error";
+#         print( OUT "Error : Unable to reach $sites{$artID} \n" );
+#         close(OUT);
+#     }
+# }
 
 foreach my $tag ( sort keys(%ptree) ) {
     my $addTag = Bookmarks::Tag->insert(
